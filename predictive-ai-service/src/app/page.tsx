2e007db97ea1bf816ef0e56c00d41975ee41b7ce
JonathanSolvesProblems <jon_andrei04@hotmail.com>
2025-04-28 05:12:06 -0400

    updated name again.

--- predictive-ai-service/src/app/page.tsx
+++ predictive-ai-service/src/app/page.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import { setUser, setToken, setPatientId } from "./redux/authSlice"; // Adjust path to redux slice
 import { handleCallback, handleLaunch } from "../utils/auth"; // Assuming these functions are implemented
 import axios from "axios";
-import { SimulateLastAnalyzed } from "@/components/SimulateLastanalyzed";
+import { SimulateLastAnalyzed } from "@/components/SimulateLastAnalyzed";
 // import AnalyzeDocumentsButton from "@/components/AnalyzeDocumentsButton";
 // import { DocumentWheel } from "@/components/DocumentWheel";
 // import Dashboard from "@/components/Dashboard";
